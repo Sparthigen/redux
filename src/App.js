388b49a5e14ed3fbe138a4700a79112587c535f3
Ahmed Dhaouadi <dapbarka@gmail.com>
2020-11-26 11:23:17 +0100

    'redux_commit'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
+import AddTask from './components/AddTask';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import ListTask from './components/ListTask';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App col-5 m-5'>
+      <AddTask/>
+      <ListTask/>
     </div>
+
+
+
+
+
+
+
+   
   );
 }
 
